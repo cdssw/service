7425cd4b291d423a8ecb0525a515b5dc2ebb26ac
cdssw <cdssw@navercom>
2025-08-02 17:24:27 +0900

    v1 add

---
+++ code_service_v1.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  // .open-window 클래스를 가진 요소를 클릭하면 새 창을 엽니다.
+  $('.hometax').on('click', function(e) {
+    e.preventDefault();
+    const url = $(this).attr('href') || $(this).data('url') || 'www.hometax.go.kr';
+    window.location.href = url;
+  });
+});
